6b596794514d304309cce89ed01abb9c34ab0755
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2015-09-23 10:48:37 -0700

    Fixed issue on Marshmallow (Android 6.0) at end animation restore

--- library/src/main/java/com/andexert/library/RippleView.java
+++ library/src/main/java/com/andexert/library/RippleView.java
@@ -146,51 +146,57 @@ public class RippleView extends RelativeLayout {
 
     @Override
     public void draw(Canvas canvas) {
-        super.draw(canvas);
-        if (animationRunning) {
-            if (DURATION <= timer * FRAME_RATE) {
-                if (mAnimationListener != null) {
-                    mAnimationListener.onRippleAnimationEnd();
+        try {
+            super.draw(canvas);
+            if (animationRunning) {
+                if (DURATION <= timer * FRAME_RATE) {
+                    if (mAnimationListener != null) {
+                        mAnimationListener.onRippleAnimationEnd();
+                    }
+                    animationRunning = false;
+                    timer = 0;
+                    durationEmpty = -1;
+                    timerEmpty = 0;
+                    canvas.restore();
+                    invalidate();
+                    return;
+                } else
+                    canvasHandler.postDelayed(runnable, FRAME_RATE);
+
+                if (timer == 0)
+                    canvas.save();
+
+
+                canvas.drawCircle(x, y, (radiusMax * (((float) timer * FRAME_RATE) / DURATION)), paint);
+
+                paint.setColor(getResources().getColor(android.R.color.holo_red_light));
+
+                if (rippleType == 1 && originBitmap != null && (((float) timer * FRAME_RATE) / DURATION) > 0.4f) {
+                    if (durationEmpty == -1)
+                        durationEmpty = DURATION - timer * FRAME_RATE;
+
+                    timerEmpty++;
+                    final Bitmap tmpBitmap = getCircleBitmap((int) ((radiusMax) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty))));
+                    canvas.drawBitmap(tmpBitmap, 0, 0, paint);
+                    tmpBitmap.recycle();
                 }
-                animationRunning = false;
-                timer = 0;
-                durationEmpty = -1;
-                timerEmpty = 0;
-                canvas.restore();
-                invalidate();
-                return;
-            } else
-                canvasHandler.postDelayed(runnable, FRAME_RATE);
 
-            if (timer == 0)
-                canvas.save();
+                paint.setColor(rippleColor);
 
+                if (rippleType == 1) {
+                    if ((((float) timer * FRAME_RATE) / DURATION) > 0.6f)
+                        paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty)))));
+                    else
+                        paint.setAlpha(PAINT_ALPHA);
+                } else
+                    paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timer * FRAME_RATE) / DURATION))));
 
-            canvas.drawCircle(x, y, (radiusMax * (((float) timer * FRAME_RATE) / DURATION)), paint);
-
-            paint.setColor(getResources().getColor(android.R.color.holo_red_light));
-
-            if (rippleType == 1 && originBitmap != null && (((float) timer * FRAME_RATE) / DURATION) > 0.4f) {
-                if (durationEmpty == -1)
-                    durationEmpty = DURATION - timer * FRAME_RATE;
-
-                timerEmpty++;
-                final Bitmap tmpBitmap = getCircleBitmap((int) ((radiusMax) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty))));
-                canvas.drawBitmap(tmpBitmap, 0, 0, paint);
-                tmpBitmap.recycle();
+                timer++;
             }
-
-            paint.setColor(rippleColor);
-
-            if (rippleType == 1) {
-                if ((((float) timer * FRAME_RATE) / DURATION) > 0.6f)
-                    paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty)))));
-                else
-                    paint.setAlpha(PAINT_ALPHA);
-            } else
-                paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timer * FRAME_RATE) / DURATION))));
-
-            timer++;
+        } catch (RuntimeException e) {
+            if (mAnimationListener != null) {
+                mAnimationListener.onRippleAnimationEnd();
+            }
         }
     }
 
